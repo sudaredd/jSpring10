35f92cf912cb23d13547d12795e9c22c018237f7
sudaredd <sudaredd@gmail.com>
2018-04-07 14:03:56 -0400

    initial commit

---
+++ src/main/java/com/jSpring10/JSpring10Application.java
@@ -0,0 +1,12 @@
+package com.jSpring10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JSpring10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JSpring10Application.class, args);
+	}
+}

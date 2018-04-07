ebbfbff8aa8efe6521957baa3a24fc8fc38ea034
sudaredd <sudaredd@gmail.com>
2018-04-07 18:01:13 -0400

    commit in branch

--- src/main/java/com/jSpring10/JSpring10Application.java
+++ src/main/java/com/jSpring10/JSpring10Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JSpring10Application {
 
 	public static void main(String[] args) {
+		System.out.println("commit in branch1");
 		SpringApplication.run(JSpring10Application.class, args);
 	}
 }
